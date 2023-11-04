0314ec45a3e19286fa5d1ae4e74f2c68fdb335b0
anonboy96 <118814879+anonboy96@users.noreply.github.com>
2023-11-04 15:58:34 +0530

    Update html_generator.py

--- html_generator.py
+++ html_generator.py
@@ -264,7 +264,7 @@ def generate_table_from_json(json_obj):
         </head>
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#"><i class="fa fa-envelope"></i> Email Analyzer</a>
+            <a class="navbar-brand" href="#"><i class="fa fa-envelope"></i> Email Header</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
